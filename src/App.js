cf727b095e515441ee91de99c8e519c3e6d483d8
unknown <lalith.chakradhar21@gmail.com>
2024-01-28 00:02:35 +0530

    XLogin done

--- src/App.js
+++ src/App.js
@@ -1,66 +1,54 @@
-import { useState } from "react";
+import React, { useState } from 'react'
 
-export default function App() {
-  const [name, setName] = useState('');
+function App()
+{
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  const [err, setErr] = useState(false);
+  const [msg, setMsg] = useState('');
+  const [isloggedin, setIsloggedin] = useState(false);
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
+
+  const handleUname = (e) => {
+    setUsername(e.target.value);
+    
   };
-
-  const handlePasswordChange = (e) => {
+  const handlepwd = (e) => {
     setPassword(e.target.value);
+   
   };
 
-  const submitHandler = (e) => {
+  const handlesubmit =(e) => {
     e.preventDefault();
-    if (name === "user" && password === "password") {
-      setErr(true);
-      setIsSubmitted(true);
-    } else {
-      setErr(false);
-      setIsSubmitted(true);
+    if(username === 'user' && password === 'password')
+    {
+       setMsg('Welcome, user!');
+       setIsloggedin(true);
     }
-  };
+    else{
+        setMsg('Invalid username or password');
+    }
+  }
 
-
-  return (
-    <>
-    <h1>Login Page</h1>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="username">
-          UserName
-          <input
-            id="username"
-            onChange={handleNameChange}
-            name="username"
-            type="text"
-            placeholder="username"
-            required
-          />
-        </label>
-        <br />
-        <br />
-        <label htmlFor="password">
-          Password
-          <input
-            id="password"
-            name="password"
-            onChange={handlePasswordChange}
-            type="password"
-            placeholder="password"
-            required
-          />
-        </label>
-        <br />
-        <br />
-        <button>Submit</button>
-      </form>
-      {isSubmitted && err && <p>Welcome, user!</p>}
-      {isSubmitted && !err && <p>Invalid username or password</p>}
-    </>
+  return(
+    <div>
+        <h1>Login Page</h1>
+        {isloggedin ? (
+            <p>{msg}</p>
+        ) :(
+            <div>
+            <p>{msg}</p>
+        <form onSubmit={handlesubmit}>
+            <label>Username:</label>
+            <input type='text' value={username} placeholder='username'  onChange={handleUname} required/>
+            <br/>
+            <label>Password:</label>
+            <input type='password' value={password} placeholder='password' onChange={handlepwd} required/>
+            <br />
+            <button type='submit'>Submit</button>
+        </form>
+        </div>
+        )}
+    </div>
   );
 }
-
+export default App;
